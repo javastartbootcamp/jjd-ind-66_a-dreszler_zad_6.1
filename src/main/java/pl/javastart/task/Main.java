b66e192d57c78fb994926ac94906f5a60fe7f117
Aleksander Dreszler <olek.d93@gmail.com>
2023-02-24 17:33:38 +0100

    Wykonano zadanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,24 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int number = 0;
+        do {
+            System.out.println("Podaj liczbę");
+            number = input.nextInt();
+            if (number < 100) {
+                System.out.println("Podana liczba jest za mała");
+            } else if (number > 200) {
+                System.out.println("Podana liczba jest za duża");
+            } else if (number % 3 != 0) {
+                System.out.println("Podana liczba nie jest podzielna przez 3");
+            } else {
+                System.out.println("Podana liczba jest ok :)");
+            }
+        } while (!(number >= 100 && number <= 200 && number % 3 == 0));
     }
 }
